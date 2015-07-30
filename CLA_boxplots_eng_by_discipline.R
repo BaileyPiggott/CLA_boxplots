30e29bee7588a9b02be48c169312abbadfefd6e9
Bailey <baileypiggott@gmail.com>
2015-07-30 11:33:48 -0400

    Engineering discipline specific CLA boxplots

---
+++ CLA_boxplots_eng_by_discipline.R
@@ -0,0 +1,96 @@
+# CLA boxplots by engineering discipline
+
+source("setup_eng_CLA.R")
+
+# CHOOSE DISICPLINE -------------------------------------------------------
+
+  df = mech
+  disc_name = "Mechanical Engineering" # text string for title and legend
+
+  df = elec
+  disc_name = "Electrical Engineering"
+
+  df = cmpe
+  disc_name = "Computer Engineering"
+  dummy <- dummy_4 %>% mutate(plan = "CMPE")
+
+  df = civl
+  disc_name = "Civil Engineering"
+
+  df = chem
+  disc_name = "Chemical Engineering"
+
+  df = ench
+  disc_name = "Engineering Chemistry"
+  dummy <- dummy_4 %>% mutate(plan = "ENCH")
+
+  df = mine
+  disc_name = "Mining Engineering"
+  dummy <-dummy_4 %>% mutate(plan = "MINE")
+
+  df = geoe
+  disc_name = "Geological Engineering"
+
+  df = enph
+  disc_name = "Engineering Physics"
+  dummy <- bind_rows(dummy_1, dummy_2) %>% mutate(plan = "ENPH")
+
+  df = mthe
+  disc_name = "Math and Engineering"
+  dummy <- dummy_4 %>% mutate(plan = "MTHE")
+
+# CREATE PLOT -------------------------------------------------------------
+
+#calculate sample sizes:
+n_1 <-  sum(with(df, year == 1 & score_total >= 1), na.rm = TRUE)     
+year1 <- paste0("First Year\nn = ", n_1, "   n = ", n_eng_1) #text string for xlabel including sample size
+n_2 <-  sum(with(df, year == 2 & score_total >= 1), na.rm = TRUE)     
+year2 <- paste0("Second Year\nn = ", n_2, "   n = ", n_eng_2) #text string for xlabel
+n_3 <-  sum(with(df, year == 3 & score_total >= 1), na.rm = TRUE)     
+year3 <- paste0("Third Year\nn = ", n_3, "   n = ", n_eng_3) #text string for xlabel
+n_4 <-  sum(with(df, year == 4 & score_total >= 1), na.rm = TRUE)     
+year4 <- paste0("Fourth Year\nn = ", n_4, "   n = ", n_eng_4) #text string for xlabel
+
+
+df <- rbind(df, all_eng, fix, dummy) # combine with all queens data
+graph_title <- paste0(disc_name, " CLA Scores") # graph title
+
+## plot description
+ggplot(
+  data = df, 
+  aes(x = as.factor(year), y = score_total, fill = plan)
+) +
+  geom_hline(
+    yintercept = c(963, 1097, 1232, 1367),  #boundaries for CLA mastery levels 
+    colour = "red", 
+    linetype = 'dashed'
+  ) +
+  geom_boxplot(width = 0.5) +    
+  coord_cartesian(xlim = c(0.5,6),ylim = c(600, 1650)) +
+  scale_x_discrete(labels = c(year1, year2, year3, year4)) + #text strings from above with sample sizes
+  labs(title = graph_title, x = "Year", y = "CLA+ Score")  +
+  scale_fill_manual(
+    values =  c("darkgoldenrod1", "steelblue3"),
+    labels = c(disc_name, "All Engineering")
+  )+
+  theme(
+    panel.border = element_rect(colour = "grey", fill = NA), #add border around graph
+    panel.background = element_rect("white"), #change background colour
+    panel.grid.major.x = element_blank(), # remove vertical lines
+    panel.grid.major.y = element_blank(), # remove horizontal lines
+    panel.grid.minor.y = element_blank(),
+    legend.title = element_blank(), #remove legend title
+    plot.title = element_text(size = 15),
+    axis.title.x = element_blank(), # remove x axis title
+    axis.title.y = element_text(size = 14),
+    axis.text = element_text(size = 12) #size of x axis labels
+  ) +
+  annotate( # add labels for CLA mastery levels
+    "text", 
+    fontface = "bold", 
+    size = 4,
+    alpha = c(0.5, 0.5,0.5,0.5,0.5, 0.8), # transparency (0-1)
+    x = 5.2, y = c(900, 1030, 1164, 1300, 1430, 1530), 
+    label = c("Below Basic", "Basic", "Proficient", "Accomplished", "Advanced", "CLA Mastery Levels"), 
+    colour = "red"
+  ) 
---
+++ setup_eng_CLA.R
@@ -0,0 +1,100 @@
+
+# set up engineering CLA data frames
+
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(magrittr)
+library(grid)# load libraries
+
+## data
+scores = read.csv("Engineering CLA+ data.csv") # scores
+ids = read.csv("Engineering Student Info.csv") #identification
+
+
+#merge data frames together
+eng_cla <- merge(scores, ids, by = c("year", "studentid")) # year and student id are the only common variables between dataframes
+eng_cla <- eng_cla[c("studentid", "score_total", "semester", "plan", "course")] #keep only important columns
+eng_cla <- eng_cla %>% mutate(year = ceiling(semester/2)) %>% arrange(plan)#add column with year calculated from semester
+
+first_year <- eng_cla %>% subset(semester <= 2) #first year scores
+
+
+#separate into disciplines
+# subset by dicsipline in "plan" column, then add first years with matching student numbers
+
+
+fix <- data.frame(c(1,2,3,4),  c(10,10,10,10),c(NA,NA,NA,NA),c(NA,NA,NA,NA),c(NA,NA,NA,NA), c(1,2,3,4))
+colnames(fix) <- colnames(eng_cla)
+
+#mech
+mech <- eng_cla %>% subset(plan == "MECH-M-BSE") #second and fourth year mech scores
+first_year_mech <- semi_join(first_year, mech, by = "studentid") #first years with matching ids to mech
+mech <- rbind(mech, first_year_mech) %>% arrange(studentid) %>% mutate(plan = "MECH")
+
+#elec
+elec <- eng_cla %>% subset(plan == "ELEC-M-BSE") #second and fourth year scores
+first_year_elec <- semi_join(first_year, elec, by = "studentid") #first years with matching ids
+elec <- rbind(elec, first_year_elec) %>% mutate(plan = "ELEC")
+
+#comp
+cmpe <- eng_cla %>% subset(plan == "CMPE-M-BSE") #second and fourth year scores
+first_year_cmpe <- semi_join(first_year, cmpe, by = "studentid") #first years with matching ids
+cmpe <- rbind(cmpe, first_year_cmpe) %>% mutate(plan = "CMPE")
+
+#civl
+civl <- eng_cla %>% subset(plan == "CIVL-M-BSE") #second and fourth year scores
+first_year_civl <- semi_join(first_year, civl, by = "studentid") #first years with matching ids
+civl <- rbind(civl, first_year_civl) %>% mutate(plan = "CIVL")
+
+#chem
+chem <- eng_cla %>% subset(plan == "CHEE-M-BSE") #second and fourth year scores
+first_year_chem <- semi_join(first_year, chem, by = "studentid") #first years with matching ids
+chem <- rbind(chem, first_year_chem) %>% mutate(plan = "CHEM")
+
+#eng chem
+ench <- eng_cla %>% subset(plan == "ENCH-M-BSE") #second and fourth year scores
+first_year_ench <- semi_join(first_year, ench, by = "studentid") #first years with matching ids
+ench <- rbind(ench, first_year_ench) %>% mutate(plan = "ENCH")
+
+#mining
+mine <- eng_cla %>% subset(plan == "MINE-M-BSE") #second and fourth year scores
+first_year_mine <- semi_join(first_year, mine, by = "studentid") #first years with matching ids
+mine <- rbind(mine, first_year_mine) %>% mutate(plan = "MINE")
+
+#geo 
+geoe <- eng_cla %>% subset(plan == "GEOE-M-BSE") #second and fourth year scores
+first_year_geoe <- semi_join(first_year, geoe, by = "studentid") #first years with matching ids
+geoe <- rbind(geoe, first_year_geoe) %>% mutate(plan = "GEOE")
+
+#eng phys
+enph <- eng_cla %>% subset(plan == "ENPH-M-BSE") #second and fourth year scores
+first_year_enph <- semi_join(first_year, enph, by = "studentid") #first years with matching ids
+enph <- rbind(enph, first_year_enph)%>% mutate(plan = "ENPH")
+
+#apple math
+mthe<- eng_cla %>% subset(plan == "MTHE-M-BSE") #second and fourth year scores
+first_year_mthe <- semi_join(first_year, mthe, by = "studentid") #first years with matching ids
+mthe <- rbind(mthe, first_year_mthe) %>% mutate(plan = "MTHE")
+
+# all eng 
+all_eng <- eng_cla %>% mutate(plan = "z_ENG") 
+
+# all eng sample sizes:
+
+n_eng_1 <-  sum(with(all_eng, year == 1 & score_total > 1), na.rm = TRUE)  
+n_eng_2 <-  sum(with(all_eng, year == 2 & score_total > 1), na.rm = TRUE) 
+n_eng_3 <-  sum(with(all_eng, year == 3 & score_total > 1), na.rm = TRUE) 
+n_eng_4 <-  sum(with(all_eng, year == 4 & score_total > 1), na.rm = TRUE) 
+
+
+# dummy data for 1st, 2nd, 4th year to fix box widths
+dummy <- fix # default to null data if discipline has data for all years
+dummy_1 <- data.frame(NA, 60, NA, NA, NA, 1)
+colnames(dummy_1) <- colnames(fix)
+dummy_2 <- data.frame(NA, 60, NA, NA, NA, 2)
+colnames(dummy_2) <- colnames(fix)
+dummy_4 <- data.frame(NA, 60, NA, NA, NA, 4)
+colnames(dummy_4) <- colnames(fix)
+
+
